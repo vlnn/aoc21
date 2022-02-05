10f20b40e7fcf8b2b909fb1335a38d58d7f8a4df
va <anohin.va@gmail.com>
2022-02-05 15:16:14 +0200

    First aoc day

--- src/aoc21/core.clj
+++ src/aoc21/core.clj
@@ -1,7 +1,39 @@
 (ns aoc21.core
+  (:require [clojure.string :as s])
   (:gen-class))
+
+(def report-data
+  (map #(Integer/parseInt %)
+       (s/split-lines
+        (slurp "resources/day1-input.txt"))))
+
+(defn depth-increases
+  "Return depths increases"
+  [xs]
+  (map #(- (second %) (first %)) (partition 2 1 xs)))
+
+(defn count-the-positives
+  "Count just positive elements of the sequence"
+  [xs]
+  (count (filter pos? xs)))
+
+(defn sweep-the-data
+  "Count positive results of sonar discrimination"
+  [xs]
+  (count-the-positives (depth-increases xs)))
+
+(defn sum-sliding-windows-by-3
+  "Return sums of sliding windows with length 3"
+  [xs]
+  (map #(reduce + %) (partition 3 1 xs)))
+
+(defn sweep-the-windows
+  "Count positive results of sonar window discrimination"
+  [xs]
+  (count-the-positives (depth-increases (sum-sliding-windows-by-3 xs))))
 
 (defn -main
   "Day 1: Sonar Sweep"
   [& args]
-  (println "Hello, World!"))
+  (println (str "Part 1: " (sweep-the-data report-data)))
+  (println (str "Part 2: " (sweep-the-windows report-data))))
--- test/aoc21/core_test.clj
+++ test/aoc21/core_test.clj
@@ -1,7 +1,16 @@
 (ns aoc21.core-test
   (:require [clojure.test :refer :all]
-            [aoc21.core :refer :all]))
+            [aoc21.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-count-the-positives
+  (testing "How many positives in sequence"
+    (is (= 3 (sut/count-the-positives [1 2 0 3 -1 -3])))
+    (is (= 0 (sut/count-the-positives [0 -2.4 -1 -3])))
+    (is (= 0 (sut/count-the-positives [])))))
+
+(deftest test-depth-increases
+  (testing "How depths's increases are calculated"
+    (is (=  () (sut/depth-increases [])))
+    (is (= [0] (sut/depth-increases [1 1])))
+    (is (= [0 1] (sut/depth-increases [1 1 2])))
+    (is (= [0 0 -1] (sut/depth-increases [1 1 1 0])))))
