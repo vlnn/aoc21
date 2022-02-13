a1bfc8fb605b49668f301747e809e49bf70e1bb7
va <anohin.va@gmail.com>
2022-02-13 22:24:48 +0200

    starting day3

--- src/aoc21/core.clj
+++ src/aoc21/core.clj
@@ -1,7 +1,15 @@
 (ns aoc21.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
+(def diagnostic-data
+  (apply map vector (map char-array (str/split-lines (slurp "resources/input.txt")))))
+
+(defn gamma-rate
+  [data]
+  ({false 0, true 1} (< (second (apply map vector (vec (frequencies (first data))))))))
+
 (defn -main
-  "Day 1: Sonar Sweep"
+  "Day 3: Binary Diagnostic"
   [& args]
   (println "Hello, World!"))
